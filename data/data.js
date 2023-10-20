d90543f1dce92b6781ff34606bd5e6c7dbb6bee0
aditakbars <aditakbars33@gmail.com>
2023-10-20 14:21:03 +0700

    fixing the NIM

--- data/data.js
+++ data/data.js
@@ -9,18 +9,18 @@ export const data = [
         id: 2,
         imageUrl: "https://source.unsplash.com/720x720?singer",
         nama: "Fadlil Ferdiansyah",
-        nim: "21120121",
+        nim: "21120121130073",
     },
     {
         id: 3,
         imageUrl: "https://source.unsplash.com/720x720?drummer",
         nama: "Muhammad Fathan Mubiina",
-        nim: "21120121",
+        nim: "211201211140164",
     },
     {
         id: 4,
         imageUrl: "https://source.unsplash.com/720x720?stud",
         nama: "Muhammad Fadlan Daris",
-        nim: "21120121",
+        nim: "21120121140146",
     },
 ];
