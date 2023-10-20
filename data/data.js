9e7eebdd0955524b041e4e173cfb8994e9f6bcc9
aditakbars <aditakbars33@gmail.com>
2023-10-20 14:06:18 +0700

    first commit

---
+++ data/data.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        id: 1,
+        imageUrl: "https://source.unsplash.com/720x720?band",
+        nama: "Aditya Akbar Subakti",
+        nim: "21120121130041",
+    },
+    {
+        id: 2,
+        imageUrl: "https://source.unsplash.com/720x720?singer",
+        nama: "Fadlil Ferdiansyah",
+        nim: "21120121",
+    },
+    {
+        id: 3,
+        imageUrl: "https://source.unsplash.com/720x720?drummer",
+        nama: "Muhammad Fathan Mubiina",
+        nim: "21120121",
+    },
+    {
+        id: 4,
+        imageUrl: "https://source.unsplash.com/720x720?stud",
+        nama: "Muhammad Fadlan Daris",
+        nim: "21120121",
+    },
+];
