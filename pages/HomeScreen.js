6bfc0494302af212c15da8ceaa403e59d38dc3e9
aditakbars <aditakbars33@gmail.com>
2023-11-04 19:14:16 +0700

    the vertical list is overall done

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,168 +1,150 @@
-import { useState, useEffect } from "react";
-import {
-    View,
-    Text,
-    StyleSheet,
-    Image,
-    Alert,
-    TouchableOpacity,
-    SafeAreaView,
-} from "react-native";
-import axios from "axios";
-
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, ScrollView, StyleSheet, Image, Dimensions } from 'react-native';
 import Header from "../components/Header";
+import fetchDataFromAPI from "../data/animeData";
+
+const windowWidth = Dimensions.get('window').width;
+const cardWidth = windowWidth * 0.6; // Lebar card sekitar 80% dari lebar layar
+const cardHeight = cardWidth * 1.9; // Tinggi card sekitar 1.5 kali lebar card
 
 function HomeScreen() {
-    const [data, setData] = useState([]);
-    const [count, setCount] = useState(1);
+  const [animeData, setAnimeData] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-        try {
-            const response = await axios.get(
-            "https://reqres.in/api/users/" + count.toString()
-            );
-            setData(response.data.data);
-        } catch (error) {
-            Alert.alert("Gagal!", error.message);
-        }
-        }
+  useEffect(() => {
+    fetchDataFromAPI().then((data) => {
+      setAnimeData(data);
+    });
+  }, []);
 
-        fetchData();
-    }, [count]);
-
-    const prevButtonHandler = () => {
-        const prevCount = count - 1;
-        setCount(prevCount);
-    };
-
-    const nextButtonHandler = () => {
-        const nextCount = count + 1;
-        setCount(nextCount);
-    };
-
-    return (
-        <SafeAreaView style={styles.container}>
-        <Header
-            headerIcon={"bell-o"}
-            headerText={"Hi, Adit!"}
-            flexPosition={"center"}
-        />
-        <View
-            style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            }}
-        >
+  return (
+    <View style={styles.container}>
+      <Header
+        headerIcon={"bell-o"}
+        headerText={"Hi, Adit dari Kelompok 17!"}
+        flexPosition={"center"}
+      />
+      <Text style={styles.title}>Top Anime</Text>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {animeData.map((item) => (
+          <View key={item.mal_id} style={styles.itemContainer}>
+            <Image source={{ uri: item.images.jpg.small_image_url }} style={styles.image} />
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.rating}>Rating: {item.score}</Text>
+          </View>
+        ))}
+      </ScrollView>
+      <Text style={styles.title}>All Anime</Text>
+      <FlatList
+        data={animeData}
+        keyExtractor={(item) => item.mal_id.toString()}
+        renderItem={({ item }) => (
+          <View>
             <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-            >
+              style={{
+                  paddingVertical: 8,
+                  borderRadius: 8,
+                  flexDirection: "column",
+                  alignItems: "center",
+              }}
+              >
             <View
                 style={{
-                justifyContent: "center",
-                alignItems: "center",
-                }}
-            >
-                <Image
-                source={{
-                    uri: data.avatar,
-                }}
-                style={{ width: 100, height: 100, borderRadius: 50 }}
-                />
-                <Text
-                style={{
-                    fontSize: 24,
-                    fontWeight: "600",
-                }}
-                >
-                {data.first_name + " " + data.last_name}
-                </Text>
-                <Text
-                style={{
-                    fontSize: 16,
-                    color: "gray",
-                }}
-                >
-                {data.email}
-                </Text>
-            </View>
-            <View
-                style={{
+                borderWidth: 2,
+                borderRadius: 8,
                 flexDirection: "row",
-                justifyContent: "center",
-                gap: 12,
+                justifyContent: "flex-start",
+                alignItems: "center",
+                width: 300,
                 }}
             >
-                {count !== 1 ? (
-                <TouchableOpacity
-                    onPress={prevButtonHandler}
-                    style={{
-                    backgroundColor: "white",
-                    paddingVertical: 8,
-                    paddingHorizontal: 16,
-                    borderRadius: 12,
-                    marginTop: 24,
-                    borderWidth: 2,
-                    }}
-                >
-                    <Text
-                    style={{
-                        color: "black",
-                        fontWeight: "400",
-                        textAlign: "center",
-                        fontSize: 16,
-                    }}
-                    >
-                    Prev
-                    </Text>
-                </TouchableOpacity>
-                ) : (
-                ""
-                )}
-                {count !== 12 ? (
-                <TouchableOpacity
-                    onPress={nextButtonHandler}
-                    style={{
-                    backgroundColor: "white",
-                    paddingVertical: 8,
-                    paddingHorizontal: 16,
-                    borderRadius: 12,
-                    marginTop: 24,
-                    borderWidth: 2,
-                    }}
-                >
-                    <Text
-                    style={{
-                        color: "black",
-                        fontWeight: "400",
-                        textAlign: "center",
-                        fontSize: 16,
-                    }}
-                    >
-                    Next
-                    </Text>
-                </TouchableOpacity>
-                ) : (
-                ""
-                )}
-            </View>
+            <Image
+            source={{ uri: item.images.jpg.image_url }}
+            style={{
+                width: 100,
+                height: 150,
+                borderRightWidth: 2,
+                borderBottomLeftRadius: 6,
+                borderTopLeftRadius: 6,
+                borderColor: "black",
+            }}
+            />
+            <View
+            style={{
+                flexDirection: "column",
+                marginLeft: 16,
+                justifyContent: "center",
+                maxWidth: 180,
+            }}
+            >
+            <Text
+                style={{
+                fontSize: 18,
+                fontWeight: "600",
+                }}
+            >
+                {item.title}
+            </Text>
+            <Text
+                style={{
+                fontWeight: "400",
+                color: "gray",
+                }}
+            >
+                {item.score}
+            </Text>
             </View>
         </View>
-        </SafeAreaView>
-    );
+      </View>
+            {/* <Image source={{ uri: item.images.jpg.image_url }} style={styles.image} />
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.rating}>Rating: {item.score}</Text> */}
+          </View>
+        )}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "white",
-        padding: 16,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '600',
+    marginVertical: 16,
+  },
+  itemContainer: {
+    width: cardWidth,
+    height: cardHeight,
+    marginRight: 20,
+    marginBottom: 20,
+    borderRadius: 8,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 4,
+    backgroundColor: 'white',
+  },
+  image: {
+    width: '100%',
+    height: '90%', // Gambar akan memenuhi 70% dari tinggi card
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    padding: 8,
+  },
+  rating: {
+    fontSize: 16,
+    color: 'gray',
+    padding: 8,
+  },
 });
 
 export default HomeScreen;
